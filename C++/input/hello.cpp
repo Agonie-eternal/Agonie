dece138d4639628d8166e2bc9f7334e38aacc865
Agonie-eternal <2487331321@qq.com>
2023-08-23 10:47:57 +0800

    c

---
+++ C++/input/hello.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Description:
+ * @Author: Agonie
+ * @Date: 2023-08-21 18:56:35
+ * @LastEditTime: 2023-08-22 21:07:50
+ * @LastEditors: Agonie
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
